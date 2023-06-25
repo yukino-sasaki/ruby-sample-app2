7894f18295b3401c52393fb88faa219447599003
yukino-sasaki <yukisnowman.18@gmail.com>
2023-06-25 19:58:43 +0900

    Implement basic login

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,23 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      reset_session
+      log_in user
+      redirect_to user
+      # ユーザーログイン後にユーザー情報のページにリダイレクトする
+    else
+      # エラーメッセージを作成する
+      flash.now[:danger] = 'Invalid email/password combination'
+    render 'new', status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    log_out
+    redirect_to root_url, status: :see_other
+  end
+end
