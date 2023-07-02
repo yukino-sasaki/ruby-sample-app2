bc4110bea0356089cb2c4fbbf86d66b0859a1eeb
yukino-sasaki <yukisnowman.18@gmail.com>
2023-07-03 02:59:21 +0900

    feat: update send mail!

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,8 +18,9 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
+    puts "user_params: #{@user_params} . #{@user.save}"
     if @user.save
-      UserMailer.account_activation(@user).deliver_now
+      @user.send_activation_email
       flash[:info] = "Please check your email to activate your account."
       redirect_to root_url
     else
